d5cb7f96cdb4490dbfd7a80459fc18875cc0a471
Jonathan Niesen <jon.niesen@gmail.com>
2014-11-10 08:03:10 -0500

    feat(index.js): add route for excepting gitub pushEvents

--- index.js
+++ index.js
@@ -6,6 +6,32 @@ app.get('/', function(req, res) {
   res.send('Hello');
 });
 
+app.post('/pushEvent', function(req, res) {
+  var dd = new dogapi();
+
+  var event = {
+    'title' : 'Testing',
+    'text'  : 'Only a test.'
+  };
+
+  console.log('Event received: ', event);
+
+  dd.add_event(event, function(error, result, status_code) {
+    if (error) {
+      res.send('There was an issue creating your event.');
+      console.log('Error: ', error);
+      console.log('Status: ', status_code);
+      return;
+    }
+
+    if (status_code == 200) {
+      console.log('Result: ', result);
+      res.send('Event created');
+      return;
+    }
+  });
+});
+
 var server = app.listen(3000, function() {
   var host = server.address().address,
       port = server.address().port;
